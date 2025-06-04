caec6fad906bdb313e94682a2ddb671d75ce3a63
hauszknechtk <hauszknechtk.24d@acsjszki.hu>
2025-06-04 17:34:17 +0200

    9-b

--- troli.js
+++ troli.js
@@ -12,7 +12,7 @@ vegallomasNevek["79"] = "Keleti pályaudvar - Kárpát utca";
 vegallomasNevek["80"] = "Keleti pályaudvar - Örs vezér tere";
 vegallomasNevek["81"] = "Örs vezér tere - Fischer István utca";
 vegallomasNevek["82"] = "Uzsoki Utcai Kórház - Örs vezér tere";
-vegallomasNevek["83"] = "Fővám tér - Orczy tér"; /* véletlenül "8-b" ként commitoltam el. */
+vegallomasNevek["83"] = "Fővám tér - Orczy tér"; /* véletlenül "8-a" ként commitoltam el. */
 
 function jaratKiiras(elem, index){
 	var ujElem = document.createElement("option");
@@ -20,9 +20,8 @@ function jaratKiiras(elem, index){
     ujElem.appendChild(ujSzoveg);
     document.getElementById("jaratSzam").appendChild(ujElem);
 }
-
 function vegallomasKiiras(jarat){
-    document.getElementById("valasz").innerHTML=vegallomasNevek[jarat];
+    document.getElementById("vegallomasok").innerHTML=vegallomasNevek[jarat];
 }
 
 function fel() {
